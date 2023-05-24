a7accfd7bc1604bcdfc75811f498af12ba266c6b
brunosj <contact@landozone.net>
2023-05-24 17:47:49 +0200

    chart dynamic area fills

--- src/lib/components/EmissionsChart/EmissionsChart.svelte
+++ src/lib/components/EmissionsChart/EmissionsChart.svelte
@@ -1,12 +1,15 @@
 <script>
 	import Axis from '$components/EmissionsChart/Axis.svelte';
-	import { extent, scaleLinear, scaleTime, line, curveNatural } from 'd3';
+	import { extent, scaleLinear, scaleTime, line, curveNatural, area } from 'd3';
 	import { fly } from 'svelte/transition';
+
 	// Export props
+
 	export let datapoints;
 	export let step;
 
 	// Define stylying variables
+
 	const margin = { top: 100, bottom: 100, left: 70, right: 70 };
 	let width = 900;
 	let height = 700;
@@ -14,7 +17,9 @@
 	const innerHeight = height - margin.top - margin.bottom,
 		innerWidth = width - margin.left - margin.right;
 
-	// Function to format the date number in a date object
+	// Define util functions
+
+	// *** format the date number in a date object
 	const reformatYear = (data) => {
 		return data.map((item) => {
 			const year = new Date(item.year, 0, 1);
@@ -22,7 +27,16 @@
 		});
 	};
 
+	// *** derive the originYValue so that the areas can fill the entire space from the origin and not the min value of the dataset
+	function getY0Value() {
+		const minYValue = yScale.domain()[0];
+		const originYValue = yScale(minYValue);
+		return originYValue;
+	}
+
 	// Declare variables used in the graph
+
+	// *** line paths
 	let lineGeneratorActual = null;
 	let lineGeneratorAboveFourDegrees = null;
 	let lineGeneratorFourDegrees = null;
@@ -31,7 +45,17 @@
 	let lineGeneratorOnePointFiveDegrees = null;
 	let lineGeneratorBottomRange = null;
 
-	// Deconstruct datapoints object to extract individual arrays
+	// *** areas
+	let areaGeneratorAboveFourDegrees = null;
+	let areaGeneratorFourDegrees = null;
+	let areaGeneratorThreeDegrees = null;
+	let areaGeneratorTwoDegrees = null;
+	let areaGeneratorOnePointFiveDegrees = null;
+	let areaGeneratorBottomRange = null;
+
+	// Arrange data
+
+	// *** deconstruct datapoints object to extract individual arrays
 	const {
 		actual,
 		aboveFourDegrees,
@@ -42,7 +66,8 @@
 		bottomRange
 	} = datapoints;
 
-	// Reformat dates in arrays and add actual data last value (this allows the line paths to start from this value rather than their own first value)
+	// *** reformat dates in arrays and add actual data last value
+	// *** (this allows the line paths to start from this value rather than their own first value)
 	const reformattedActual = reformatYear(actual);
 	const lastActualData = reformattedActual[reformattedActual.length - 1];
 
@@ -61,7 +86,7 @@
 		reformattedArrays[`reformatted${arrayObj.name}`] = reformattedArray;
 	}
 
-	// Merge arrays to derive full range of values
+	// *** merge arrays to derive full range of values
 	const mergedArray = [
 		...reformattedActual,
 		...reformattedArrays.reformattedAboveFourDegrees,
@@ -88,8 +113,9 @@
 		.range([innerHeight, 0])
 		.nice();
 
-	// Use reactive variable to render line paths
+	// Use reactive variables to render line paths and areas
 
+	// *** line paths
 	$: lineGeneratorActual = line()
 		.curve(curveNatural)
 		.x((d) => xScale(d.year))
@@ -124,7 +150,46 @@
 		.curve(curveNatural)
 		.x((d) => xScale(d.year))
 		.y((d) => yScale(d.emissions))(reformattedArrays.reformattedBottomRange);
+
+	// *** areas
+	$: areaGeneratorAboveFourDegrees = area()
+		.curve(curveNatural)
+		.x((d) => xScale(d.year))
+		.y0(() => getY0Value())
+		.y1((d) => yScale(d.emissions))(reformattedArrays.reformattedAboveFourDegrees);
+
+	$: areaGeneratorFourDegrees = area()
+		.curve(curveNatural)
+		.x((d) => xScale(d.year))
+		.y0(() => getY0Value())
+		.y1((d) => yScale(d.emissions))(reformattedArrays.reformattedFourDegrees);
+
+	$: areaGeneratorThreeDegrees = area()
+		.curve(curveNatural)
+		.x((d) => xScale(d.year))
+		.y0(() => getY0Value())
+		.y1((d) => yScale(d.emissions))(reformattedArrays.reformattedThreeDegrees);
+
+	$: areaGeneratorTwoDegrees = area()
+		.curve(curveNatural)
+		.x((d) => xScale(d.year))
+		.y0(() => getY0Value())
+		.y1((d) => yScale(d.emissions))(reformattedArrays.reformattedTwoDegrees);
+
+	$: areaGeneratorOnePointFiveDegrees = area()
+		.curve(curveNatural)
+		.x((d) => xScale(d.year))
+		.y0(() => getY0Value())
+		.y1((d) => yScale(d.emissions))(reformattedArrays.reformattedOnePointFiveDegrees);
+
+	$: areaGeneratorBottomRange = area()
+		.curve(curveNatural)
+		.x((d) => xScale(d.year))
+		.y0(() => getY0Value())
+		.y1((d) => yScale(d.emissions))(reformattedArrays.reformattedBottomRange);
 </script>
+
+// HTML used to render the chart
 
 <div class="chart-container">
 	<svg {width} {height}>
@@ -135,9 +200,12 @@
 				Emissions excl. LULUCF (MtCO2e/year)
 			</text>
 
+			// Historical data graph
 			{#each reformattedActual as data, i}
 				<path d={lineGeneratorActual} fill="none" stroke="#440FDB" stroke-width="2.5" />
 			{/each}
+
+			// Pathways graph
 			{#each Object.entries(reformattedArrays) as [key, dataArray], i}
 				{@const colorCodes = {
 					reformattedAboveFourDegrees: '#FFF',
@@ -155,8 +223,21 @@
 					reformattedOnePointFiveDegrees: lineGeneratorOnePointFiveDegrees,
 					reformattedBottomRange: lineGeneratorBottomRange
 				}}
+
+				{@const areaGenerators = {
+					reformattedAboveFourDegrees: areaGeneratorAboveFourDegrees,
+					reformattedFourDegrees: areaGeneratorFourDegrees,
+					reformattedThreeDegrees: areaGeneratorThreeDegrees,
+					reformattedTwoDegrees: areaGeneratorTwoDegrees,
+					reformattedOnePointFiveDegrees: areaGeneratorOnePointFiveDegrees,
+					reformattedBottomRange: areaGeneratorBottomRange
+				}}
+
 				{#if step >= i + 1}
-					<g out:fly={{ duration: 400, delay: i * 15 }}>
+					<g
+						out:fly={{ duration: 400, delay: i * 15 }}
+						in:fly={{ duration: 1000, y: 200, opacity: 0 }}
+					>
 						{#each dataArray as data}
 							<path
 								d={lineGenerators[key]}
@@ -164,6 +245,7 @@
 								stroke={colorCodes[key]}
 								stroke-width="2.5"
 							/>
+							<path d={areaGenerators[key]} fill={colorCodes[key]} />
 						{/each}
 					</g>
 				{/if}
@@ -178,8 +260,7 @@
 	text {
 		fill: white;
 	}
-	path {
-		fill: transparent;
+	/* path {
 		stroke-width: 2.5;
 		stroke-linejoin: round;
 		stroke-dasharray: 4400;
@@ -194,5 +275,5 @@
 		to {
 			stroke-dashoffset: 0;
 		}
-	}
+	} */
 </style>
